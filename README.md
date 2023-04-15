6153416f99ab62cb10e2749e9fe6fa16ebd1f8cf
theonetestbot <129641862+theonetestbot@users.noreply.github.com>
2023-04-15 21:22:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,71 @@
+<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
+
+#  THE RENAME BOT
+
+
+<p align="center"> 🅡🅔🅟🅞 🅢🅣🅐🅣🅢 </p>
+
+
+![github card](https://github-readme-stats.vercel.app/api/pin/?username=theonerips&repo=renamebot&theme=dark)
+
+
+### SAMPLE BOT (official pyro rename bot)
+
+<p align="center">
+🤖 <a href="https://t.me/theone_anime"><img title="Telegram" src="https://img.shields.io/static/v1?label=PYRO+RENAME&message=BOT&color=blue-green"></a> 🤖
+</p>
+
+
+## Deploy To Koyeb
+
+[![Deploy to Koyeb](https://www.koyeb.com/static/images/deploy/button.svg)](https://app.koyeb.com/deploy?type=git&repository=github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT&env[BOT_TOKEN]&env[API_ID]&env[API_HASH]&env[WEBHOOK]=True&env[ADMIN]&env[DB_URL]&env[DB_NAME]=pyro-botz&env[FORCE_SUB]&env[START_PIC]&env[PORT]=8080&run_command=python%20bot.py&branch=main&name=pyro-rename) 
+
+## Deploy To Render
+<!--
+◉ watch tutorial for render deploy <a href="https://youtu.be/i6OX3xzOCGY"><img src="https://img.shields.io/badge/Watch%20Tutorial%20On%20YouTube-red.svg?logo=Youtube"></a> -->                 
+
+[![Deploy to Render](https://render.com/images/deploy-to-render-button.svg)](https://render.com/deploy?repo=https://github.com/theonerips/renamebot)
+
+## Deploy To Railway
+
+<a href="https://graph.org/file/fabd75cd5043d2cfdc13d.jpg"><img src="https://railway.app/button.svg" alt="Deploy"></a>
+
+## Deploy To Heroku
+
+<a href="https://heroku.com/deploy?template=https://github.com/theonerips/renamebot"><img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy"></a>
+
+
+
+## Configs 
+
+* `BOT_TOKEN`  - Get bot token from @BotFather
+
+* `API_ID` - From my.telegram.org 
+
+* `API_HASH` - From my.telegram.org
+
+* `WEBHOOK` - if your server is need web service! value = `True` else value = `False`
+
+* `ADMIN` - AUTH or bot controllers id's multiple id use space to split 
+
+* `DB_URL`  - Mongo Database URL from https://cloud.mongodb.com/
+
+* `DB_NAME`  - Your database name from mongoDB. Default will be 'my'
+
+* `FORCE_SUB` - your force sub channel username without @ 
+
+* `START_PIC` - start message photo
+
+## Botfather Commands
+```
+start - bot alive cheking
+viewthumb - View Thumbnail
+delthumb - Delete Thumbnail
+set_caption - set a custom caption
+see_caption - see your custom caption
+del_caption - delete custom caption
+users - admin only
+broadcast - admin only
+```
+
+
---
+++ helper/txt.py
@@ -0,0 +1,72 @@
+"""
+Apache License 2.0
+
+Copyright (c) 2022
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+class mr(object):
+    PROGRESS_BAR = """\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗂️ : {1} | {2}
+┣⪼ ⏳️ : {0}%
+┣⪼ 🚀 : {3}/s
+┣⪼ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━➣ """
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+├👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂 : <a href=https://t.me/amoltheone>𝐓𝐡𝐞𝐨𝐧𝐞</a> 
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾𝙳𝙱</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v2.1.90 [ ꜱɪɢᴍᴀ 🗿 ]              
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO SET THUMBNILE</u></b>
+  
+•> /start a bot and send any picture to automatically set thumbnile.
+•> /delthumb use this command and delete your old thumbnile.
+•> /viewthumb use this command view your current thumbnile.
+
+📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
+•> /set_caption - set a custom caption
+•> /see_caption - see your custom caption
+•> /del_caption - delete custom caption
+
+Example:- /set_caption 📕 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+✏️ <b><u>HOW TO RENAME A FILE</u></b>
+•> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
+ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/amoltheone>𝑺𝑼𝑷𝑷𝑶𝑹𝑻 𝑮𝑹𝑶𝑼𝑷</a>
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<u><b>SUPPORTS</b></u> 
+
+<b><u>Special Thanks & Developers</b></u> 
+
+» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://github.com/theonetestbot/renameplus>𝐓𝐇𝐄 𝐑𝐄𝐍𝐀𝐌𝐄 𝐁𝐎𝐓</a>
+» 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 : <a href=https://www.youtube.com/c/AmolAdhude>THE ONE</a>
+
+
